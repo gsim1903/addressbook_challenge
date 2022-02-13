aad62736653c5636e9be2ed0c628fc6f560452de
gsim1903 <gavin@internetbusker.co>
2022-02-13 10:06:22 +0100

    create indexscript for app

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -1,6 +1,22 @@
 describe('user can create a contact', () => {
-	it('test', () => {
-		cy.visit('http://localhost:3001')
-        cy.get('#add-contact').click()
-	})
+  it('test', () => {
+    cy.visit('http://localhost:3001')
+    cy.get('#add-contact').click()
+
+    cy.get('#name').type('Thomas')
+    cy.get('#email').type('thomas@craft.se')
+    cy.get('#phone').type('0700 101010')
+    cy.get('#company').type('Craft Academy')
+    cy.get('#notes').type('Awesome coder')
+    cy.get('#twitter').type('@thomasochman')
+    cy.get('#submit').click()
+  })
+
+  it('displays a name of the new contact', () => {
+    cy.get('#contact-list').should('contain', 'Thomas')
+  })
+
+  it('displays the phone number of the new contact', () => {
+    cy.get('#contact-list').should('contain', '0700 101010')
+  })
 })
